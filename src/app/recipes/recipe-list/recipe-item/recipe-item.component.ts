eb07b692bdcd7e05d0befde83d26c433063ef6e7
meadowstream <jimmyrare@gmail.com>
2019-01-02 18:21:33 +0100

    Section 6, Lecture 82

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
-export class RecipeItemComponent implements OnInit {
+export class RecipeItemComponent {
+  @Input() recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
 
-  constructor() { }
-
-  ngOnInit() {
+  onSelected() {
+    this.recipeSelected.emit();
   }
-
 }
